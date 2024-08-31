267f6cf9ffce5fb71c27ff23f65bbaa379a45075
Abani Senapati <95952099+abanis24@users.noreply.github.com>
2024-08-31 06:28:30 +0000

    added API route and dynamic data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Flask_App
-Flask API Project
+Flask API Project - Career Website
+Building a career website where user can apply to jobs in the portal and company can post vacancies across world wide location.
+
+Tech - Python, Flask, HTML, Bootstrap 
--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS=[
+  {
+    "ID":1,
+    "Postion":"Data Analyst",
+    "Location":"Bengaluru,India",
+    "Salary":"Rs 10,00,000"
+  },
+  {
+    "ID":2,
+    "Postion":"Data Scientist",
+    "Location":"Delhi,India",
+    "Salary":"Rs 15,00,000"
+  },
+  {
+    "ID":3,
+    "Postion":"Front end Engineer",
+    "Location":"Bengaluru,India",
+    "Salary":"Rs 10,00,000"
+  },
+  {
+    "ID":1,
+    "Postion":"Backend Engineer",
+    "Location":"San Fransico,USA",
+    "Salary":"$ 10,00,000"
+  }
+]
+
 @app.route('/')
 def hello_world():
-  return "Hello World!"
+  return render_template('home.html',jobs=JOBS,company_name='LearnIT')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
